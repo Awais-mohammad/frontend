85305a8b744246ede885a1cacc57ca7f438f26b9
Awais-Mohammad <muhammadawais083@gmail.com>
2021-09-01 23:55:43 +0500

    products!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "home",
+    redirectTo: "products",
     pathMatch: "full"
   }, {
     path: "home",
