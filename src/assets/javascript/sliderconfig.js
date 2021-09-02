b746e1d474e09e9652ed8be70408bbfb06e7baf1
Awais-Mohammad <muhammadawais083@gmail.com>
2021-09-02 12:10:30 +0500

    homepagephoneresponsive!!

--- src/assets/javascript/sliderconfig.js
+++ src/assets/javascript/sliderconfig.js
@@ -1,12 +1,25 @@
+
 $(document).ready(function () {
 	$('.carousel').slick({
 		slidesToShow: 3,
-		dots: true,
-		centerMode: true,
+		adaptiveHeight: true,
 		autoplay: true,
 		arrows: true,
+		centerMode: true,
 		autoplaySpeed: 1500,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				centerMode: true,
+			},
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				centerMode: true,
+			}
+		}]
 	});
 });
-
-  // Slick version 1.5.8
