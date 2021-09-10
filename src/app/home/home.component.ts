4defa7086f9d03dfbbc5a622006035479c48df57
Awais-Mohammad <muhammadawais083@gmail.com>
2021-09-10 12:28:22 +0500

    sliderupdatehome!!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { OnDestroy } from '@angular/core';
-
+import { Component, OnInit, ElementRef, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +8,26 @@ import { OnDestroy } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private elementRef: ElementRef,
+    @Inject(DOCUMENT) private doc,
+  ) { }
 
   ngOnInit(): void {
    
+
+
+    var s14 = document.createElement("script");
+    s14.type = "text/javascript";
+    s14.src = "../assets/javascript/sliderconfig.js";
+    this.elementRef.nativeElement.appendChild(s14);
+
+  
   }
 
 
-  
-  
-  
+
+
+
 
 }
--- src/assets/javascript/sliderconfig.js
+++ src/assets/javascript/sliderconfig.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-	$('.carousel').slick({
+	$('.carousel').not('.slick-initialized').slick({
 		slidesToShow: 3,
 		adaptiveHeight: true,
 		autoplay: true,
@@ -25,43 +25,6 @@ $(document).ready(function () {
 
 
 
-	$(".slider").not('.slick-initialized').slick({
-		centerMode: true,
-		autoplay: true,
-		dots: true,
-
-		slidesToShow: 3,
-		responsive: [{
-			breakpoint: 768,
-			settings: {
-				dots: false,
-				arrows: false,
-				infinite: false,
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}]
-	});
-	$('.customer-logos').slick({
-		slidesToShow: 6,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1500,
-		arrows: false,
-		dots: false,
-		pauseOnHover: false,
-		responsive: [{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 4
-			}
-		}, {
-			breakpoint: 520,
-			settings: {
-				slidesToShow: 3
-			}
-		}]
-	});
 
 
 });
