df54bb95408953af61dd4d9ad68cdd0784bdd052
Awais-Mohammad <muhammadawais083@gmail.com>
2021-09-16 15:27:09 +0500

    localstorage!!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,6 +43,68 @@ export class HomeComponent implements OnInit {
     const dialogRef = this.dialog.open(ProdDetailComponent, dialog);
   }
 
+  temparray: any[] = [];
+  existingarray: any[] = []
+
+  addToCart(prodID) {
+
+    console.log(prodID);
+
+    this.existingarray = JSON.parse(localStorage.getItem('cart'))
+
+    if (!this.existingarray) {
+      this.temparray.push(prodID)
+      localStorage.setItem('cart', JSON.stringify(this.temparray))
+      location.reload()
+    }
+    else {
+      this.temparray.push(prodID)
+
+
+      for (var i = 0; i < this.existingarray.length; i++) {
+        console.log(this.existingarray[i]);
+        this.temparray.push(this.existingarray[i])
+
+      }
+
+
+      localStorage.setItem('cart', JSON.stringify(this.temparray))
+      location.reload()
+
+    }
+
+  }
+
+
+  addtofav(prodID) {
+
+    console.log(prodID);
+
+    this.existingarray = JSON.parse(localStorage.getItem('fav'))
+
+    if (!this.existingarray) {
+
+      this.temparray.push(prodID)
+      localStorage.setItem('cart', JSON.stringify(this.temparray))
+      location.reload()
+
+    }
+    else {
+      this.temparray.push(prodID)
+      for (var i = 0; i < this.existingarray.length; i++) {
+        console.log(this.existingarray[i]);
+        this.temparray.push(this.existingarray[i])
+
+      }
+
+      localStorage.setItem('fav', JSON.stringify(this.temparray))
+      location.reload()
+
+    }
+
+  }
+
+
   ngOnInit(): void {
 
     this.getProds()
@@ -51,6 +113,8 @@ export class HomeComponent implements OnInit {
     s14.type = "text/javascript";
     s14.src = "../assets/javascript/sliderconfig.js";
     this.elementRef.nativeElement.appendChild(s14);
+    let data = JSON.parse(localStorage.getItem('cart'))
+    console.log(data, 'check');
 
 
   }
